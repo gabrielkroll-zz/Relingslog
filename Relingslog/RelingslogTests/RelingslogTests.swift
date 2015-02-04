960bb0fc334b7163c8bd9a199dda150946e0eb06
Gabriel Kroll <gabriel.kroll@me.com>
2015-02-04 02:34:07 +0100

    Created Xcode Project

---
+++ Relingslog/RelingslogTests/RelingslogTests.swift
@@ -0,0 +1,36 @@
+//
+//  RelingslogTests.swift
+//  RelingslogTests
+//
+//  Created by Gabriel Kroll on 04/02/15.
+//  Copyright (c) 2015 Gabriel Kroll. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class RelingslogTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
